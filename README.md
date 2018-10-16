af758d33e74b46b9c9bf0ac45bd46ac6f73ef1a0
hazae41 <hazae41@gmail.com>
2018-10-16 16:30:11 +0200

    pre-2.0

--- README.md
+++ README.md
@@ -1,7 +1,136 @@
-# Kotlin4MC
-Kotlin for Minecraft
+# Kotlin4MC : The ultimate Kotlin library for Minecraft plugin development
 
-### Use it
+For Spigot and BungeeCord
+
+### Simplified componentization
+
+You can componentize and colorize any message
+
+##### With Kotlin4MC
+
+```kotlin
+text("Hello world!")
+```
+
+##### Without Kotlin4MC
+
+```kotlin
+TextComponent("Hello world".replace("&", "§"))
+```
+
+### Simplified messages sending
+
+Let's send a message to a player (any Player, ProxiedPlayer, CommandSender is supported)
+
+##### With Kotlin4MC
+
+```kotlin
+player.msg("Hello world!")
+```
+
+##### Without Kotlin4MC (deprecated)
+
+```kotlin
+player.sendMessage("Hello world!".replace("&", "§"))
+```
+
+##### Without Kotlin4MC (not deprecated)
+
+```kotlin
+player.sendMessage(TextComponent("Hello world!".replace("&", "§")))
+```
+
+### Simplified event listening
+
+You can use listen() to easily listen to events
+
+##### With Kotlin4MC
+
+```kotlin
+listen<PlayerJoinEvent>{
+  it.player.msg("Hello world!")
+}
+```
+
+##### Without Kotlin4MC
+
+```kotlin
+server.pluginManager.registerEvent(object: Listener{
+  @EventHandler
+  fun onPlayerJoin(e: PlayerJoinEvent){
+      e.player.sendMessage("Hello world!")
+  }
+}, this)
+```
+
+##### In Java
+
+```java
+getServer().getPluginManager().registerEvents(new Listener() {
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent e){
+      e.getPlayer().sendMessage("Hello world!");
+  }
+}, this);
+```
+
+You can also use priorities
+
+```kotlin
+listen<PlayerJoinEvent>(HIGHEST){
+  it.player.msg("This will be the first")
+}
+```
+
+### Simplified scheduling
+
+```kotlin
+schedule(delay = 10){
+    // this will be executed after 10 ticks
+}
+
+schedule(delay = 10, period = 20){
+    // this will be executed after 10 ticks then each 20 ticks (1 second)
+}
+
+schedule(async = true){
+    // this will be executed asynchronously now
+}
+
+schedule(true, delay = 20){
+    // this will be executed asynchronously after 20 ticks (1 second)
+}
+
+schedule(true, period = 3, unit = TimeUnit.MINUTES){
+    // this will be executed asynchronously each 3 minutes
+}
+```
+
+Warning: do not use TimeUnit.MILLISECONDS with Bukkit
+Warning: never use TimeUnit.NANOSECONDS and TimeUnit.MICROSECONDS
+
+### Plugin updates checker
+
+You can check for updates of your plugin using Spiget
+![](https://ipfs.io/ipfs/QmZU6234EqPiQVH8Wj6ofkNgsA4UuUTovaMVbp85DPuubF/Capture.PNG)
+
+Just use update() with your Spigot resource ID
+```kotlin
+update(15938, RED)
+```
+
+You can specify the color
+```kotlin
+update(15938, LIGHT_PURPLE)
+```
+
+and the permission (the default permission is "rhaz.update")
+```kotlin
+update(15938, LIGHT_PURPLE, "myplugin.updates")
+```
+
+
+### How to implement it?
 
 - Gradle: add this to your build.gradle
 
--- src/Kotlin4MC.kt
+++ src/Kotlin4MC.kt
@@ -3,11 +3,15 @@ package fr.rhaz.minecraft
 import com.google.gson.JsonParser
 import net.md_5.bungee.api.ChatColor
 import net.md_5.bungee.api.ChatColor.*
-import net.md_5.bungee.api.CommandSender
+import net.md_5.bungee.api.CommandSender as BungeeSender
 import net.md_5.bungee.api.chat.ClickEvent
+import net.md_5.bungee.api.chat.ClickEvent.Action.*
 import net.md_5.bungee.api.chat.TextComponent
 import net.md_5.bungee.api.event.PostLoginEvent
+import org.bukkit.event.Event
+import org.bukkit.event.EventPriority
 import org.bukkit.event.player.PlayerJoinEvent
+import java.io.File
 import java.io.IOException
 import java.io.InputStreamReader
 import java.net.URL
@@ -32,10 +36,31 @@ class Kotlin4Bungee: BungeePlugin(){
 @SpongePlugin(id = "kotlin4mc", name = "Kotlin4Sponge")
 class Kotlin4Sponge
 
-fun CommandSender.msg(msg: String) = msg(text(msg))
-fun CommandSender.msg(text: TextComponent) = sendMessage(text)
+// Java compat for the Unit type
+// Usage: getUnit() <=> Unit.INSTANCE
+val unit = Unit
+
+// Get the lowercase version of any String
+// Usage "HeLlO wOrLd".lc <=> *.toLowerCase() => "hello world"
+val String.lc get() = toLowerCase()
+
+// Usage: dataFolder["config.yml"] <=> File(dataFolder, "config.yml")
+operator fun File.get(key: String) = File(this, key)
+
+// Short messages sending
+// Usage: player.msg("hello world") <=> player.sendMessage(TextComponent("hello world".replace("&", "§")))
+fun BungeeSender.msg(msg: String) = msg(text(msg))
+fun BungeeSender.msg(text: TextComponent) = sendMessage(text)
+// Componentize and colorize any message
 fun text(string: String) = TextComponent(string.replace("&", "§"))
 
+// Get latest version of a plugin ID
+// What's my plugin ID?
+// Given this resource: https://www.spigotmc.org/resources/commandsdispatcher-send-commands-to-your-servers-using-sockets.9854/
+// The plugin ID is 9854
+// Usage: spiget(9854){
+//   your code using "it" as the latest version
+// }
 fun spiget(id: Int, callback: (String) -> Unit) = Thread {
     try {
         val base = "https://api.spiget.org/v2/resources/"
@@ -45,6 +70,9 @@ fun spiget(id: Int, callback: (String) -> Unit) = Thread {
     } catch (e: IOException){}
 }.start()
 
+
+// Check if a version is newer than another
+// Usage: "1.0" newerThan "0.9.5" => true
 infix fun String.newerThan(v: String): Boolean = false.also{
     val s1 = split('.');
     val s2 = v.split('.');
@@ -56,50 +84,99 @@ infix fun String.newerThan(v: String): Boolean = false.also{
     }
 }
 
-fun BukkitPlugin.update(id: Int, color: ChatColor) = spiget(id) here@{
+// Notifications of plugin updates for Bukkit
+// Usage: update(9854, RED, "myplugin.update")
+// Will check the latest version of plugin ID 9854
+// the message will be in red
+// and sent to players with permission "myplugin.update"
+fun BukkitPlugin.update(id: Int, color: ChatColor = LIGHT_PURPLE, permission: String = "rhaz.update")
+    = spiget(id) here@{
 
-    if(!(it newerThan description.version)) return@here;
+        if(!(it newerThan description.version)) return@here;
 
-    val message = text("An update is available for ${description.name}!").apply {
         val url = "https://www.spigotmc.org/resources/$id"
-        text += " Download it here: $url"
-        this.color = color
-        clickEvent = ClickEvent(ClickEvent.Action.OPEN_URL, url)
+        val message = text(
+            "An update is available for ${description.name}!" +
+            " Download it here: $url"
+        ).apply {
+            this.color = color
+            clickEvent = ClickEvent(OPEN_URL, url)
+        }
+
+        server.scheduler.runTaskLater(this, {
+            server.consoleSender.spigot().sendMessage(message);
+        }, 0)
+
+        listen<PlayerJoinEvent>{
+            if(it.player.hasPermission(permission))
+                it.player.spigot().sendMessage(message)
+        }
     }
 
-    server.scheduler.runTaskLater(this, {
-        server.consoleSender.spigot().sendMessage(message);
-    }, 0)
+// Notifications of plugin updates for BungeeCord
+// Usage: update(9854, RED, "myplugin.update")
+// Will check the latest version of plugin ID 9854
+// the message will be in red
+// and sent to players with permission "myplugin.update"
+fun BungeePlugin.update(id: Int, color: ChatColor = LIGHT_PURPLE, permission: String = "rhaz.update")
+    = spiget(id) here@{
+        if(!(it newerThan description.version)) return@here;
 
-    server.pluginManager.registerEvents(object: BukkitListener {
-        @BukkitEventHandler
-        fun onJoin(e: PlayerJoinEvent) {
-            if(e.player.hasPermission("rhaz.update"))
-                e.player.spigot().sendMessage(message)
+        val url = "https://www.spigotmc.org/resources/$id"
+        val message = text(
+            "An update is available for ${description.name}!" +
+            " Download it here: $url"
+        ).apply {
+            this.color = color
+            clickEvent = ClickEvent(OPEN_URL, url)
         }
-    }, this)
+
+        proxy.scheduler.schedule(this, {
+            proxy.console.sendMessage(message);
+        }, 0, TimeUnit.SECONDS)
+
+        proxy.pluginManager.registerListener(this, object : BungeeListener {
+            @BungeeEventHandler
+            fun onJoin(e: PostLoginEvent) {
+                if (e.player.hasPermission(permission))
+                    e.player.sendMessage(message)
+            }
+        })
+    }
+
+inline fun <reified T: Event> BukkitPlugin.listen(
+    priority: EventPriority = EventPriority.NORMAL,
+    crossinline callback: (T) -> Unit
+){
+    server.pluginManager.registerEvent(
+        T::class.java, object: BukkitListener {},
+        priority, { _, it -> callback(it as T) },
+        this
+    )
 }
 
-fun BungeePlugin.update(id: Int, color: ChatColor) = spiget(id) here@{
+fun BukkitPlugin.schedule(
+    async: Boolean = false,
+    delay: Long? = null,
+    period: Long? = null,
+    unit: TimeUnit? = null,
+    callback: () -> Unit
+){
+    if(period != null){
+        var delay = delay ?: 0
+        delay = unit?.toSeconds(delay)?.let{it*20} ?: delay
+        val period = unit?.toSeconds(period)?.let{it*20} ?: period
 
-    if(!(it newerThan description.version)) return@here;
-
-    val message = text("An update is available for ${description.name}!").apply {
-        val url = "https://www.spigotmc.org/resources/$id"
-        text += " Download it here: $url"
-        this.color = color
-        clickEvent = ClickEvent(ClickEvent.Action.OPEN_URL, url)
+        if(async) server.scheduler.runTaskTimerAsynchronously(this, callback, delay, period)
+        else server.scheduler.runTaskTimer(this, callback, delay, period)
+        return;
     }
-
-    proxy.scheduler.schedule(this, {
-        proxy.console.sendMessage(message);
-    }, 0, TimeUnit.SECONDS)
-
-    proxy.pluginManager.registerListener(this, object : BungeeListener {
-        @BungeeEventHandler
-        fun onJoin(e: PostLoginEvent) {
-            if (e.player.hasPermission("rhaz.update"))
-                e.player.sendMessage(message)
-        }
-    })
+    if(delay != null){
+        val delay = unit?.toSeconds(delay)?.let{it*20} ?: delay
+        if(async) server.scheduler.runTaskLaterAsynchronously(this, callback, delay)
+        else server.scheduler.runTaskLater(this, callback, delay)
+        return;
+    }
+    if(async) server.scheduler.runTaskAsynchronously(this, callback)
+    else server.scheduler.runTask(this, callback)
 }

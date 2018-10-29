12d97e0a315c2871a12c7bbd02df6c7ffa582271
hazae41 <hazae41@gmail.com>
2018-10-29 08:42:10 +0100

    2.0.7: fix tasks on BungeeCord

--- src/Kotlin4MC.kt
+++ src/Kotlin4MC.kt
@@ -422,23 +422,24 @@ fun BungeePlugin.schedule(
         callback: BungeeTask.() -> Unit
 ): BungeeTask {
     lateinit var task: BungeeTask
-    task =  if(period != null){
+    task =
+    if(period != null){
         var delay = delay ?: 0
         val unit = unit ?: TimeUnit.MILLISECONDS.also{ delay *= 50 }
         if(async)
             proxy.scheduler.schedule(this, {
-                proxy.scheduler.runAsync(this, {task.callback()})
+                proxy.scheduler.runAsync(this) {task.callback()}
             }, delay, period, unit)
         else proxy.scheduler.schedule(this, {task.callback()}, delay, period, unit)
     } else if(delay != null){
         var delay = delay
         val unit = unit ?: TimeUnit.MILLISECONDS.also{ delay *= 50 }
-        return if(async)
+        if(async)
             proxy.scheduler.schedule(this, {
                 proxy.scheduler.runAsync(this) {task.callback()}
             }, delay, unit)
         else proxy.scheduler.schedule(this, {task.callback()}, delay, unit)
-    } else if(async) proxy.scheduler.runAsync(this, {task.callback()})
+    } else if(async) proxy.scheduler.runAsync(this) {task.callback()}
     else proxy.scheduler.schedule(this, {task.callback()}, 0, TimeUnit.MILLISECONDS)
     return task
 }

710e963d6bc52a61fff568bdde08dd7cbef14512
hazae41 <hazae41@gmail.com>
2018-11-02 19:25:18 +0100

    2.0.8: added ignoreCancelled in BukkitPlugin.listen()

--- src/Kotlin4MC.kt
+++ src/Kotlin4MC.kt
@@ -306,39 +306,44 @@ fun BukkitConfigurationSection.section(path: String) = getConfigurationSection(p
 val BukkitConfigurationSection.sections get() = keys.map{section(it)}
 
 // ----------------------------- LISTENERS -----------------------------
-inline fun <reified T: BukkitEvent> BukkitPlugin.listen(
-        priority: BukkitEventPriority = BukkitEventPriority.NORMAL,
-        crossinline callback: (T) -> Unit
-){
-    server.pluginManager.registerEvent(
-        T::class.java, object: BukkitListener {},
-        priority, { _, it -> callback(it as T) },
-        this
-    )
-}
 
+@JvmOverloads
+inline fun <reified T: BukkitEvent> BukkitPlugin.listen(
+    priority: BukkitEventPriority = BukkitEventPriority.NORMAL,
+    ignoreCancelled: Boolean = false,
+    crossinline callback: (T) -> Unit
+) = server.pluginManager.registerEvent(
+    T::class.java, object: BukkitListener {},
+    priority, { _, it -> callback(it as T) },
+    this, ignoreCancelled
+)
+
+@JvmOverloads
 inline fun <reified T: BungeeEvent> BungeePlugin.listen(
-        priority: Byte = BungeeEventPriority.NORMAL,
-        crossinline callback: (T) -> Unit
+    priority: Byte = BungeeEventPriority.NORMAL,
+    crossinline callback: (T) -> Unit
 ){
     val pm = proxy.pluginManager
     val pmc = pm::class.java
     val bus = pmc.getDeclaredField("eventBus").run {
         isAccessible = true; get(pm) as EventBus
     }
+
     val busc = bus::class.java
     val bLaP = busc.getDeclaredField("byListenerAndPriority").run {
         isAccessible = true; get(bus) as HashMap<Class<*>, Map<Byte, Map<Object, Array<Method>>>>
     }
+
     val priorities = bLaP[T::class.java] as? HashMap<Byte, Map<Object, Array<Method>>>
-            ?: HashMap<Byte, Map<Object, Array<Method>>>()
-                    .also { bLaP[T::class.java] = it }
+    ?: HashMap<Byte, Map<Object, Array<Method>>>().also { bLaP[T::class.java] = it }
+
     val handlers = priorities[priority] as? HashMap<Object, Array<Method>>
-            ?: HashMap<Object, Array<Method>>()
-                    .also { priorities[priority] = it }
+    ?: HashMap<Object, Array<Method>>().also { priorities[priority] = it }
+
     val listener = object: BungeeListener {
         fun onEvent(it: T) = callback(it)
     }
+
     handlers[listener as Object] = arrayOf(listener::class.java.getMethod("onEvent", BungeeEvent::class.java))
     busc.getDeclaredMethod("bakeHandlers", Class::class.java).apply {
         isAccessible = true; invoke(bus, T::class.java)
